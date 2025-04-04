1953e117328e8c9db30b70b686eaebdaa84a4f07
Rafael Ferro <rafaelferromac@gmail.com>
2025-04-04 16:22:32 -0300

    Login

---
+++ src/test/java/com/ProjectEngenhariaDeSoftwareTIN2/projectES/ProjectEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectEngenhariaDeSoftwareTIN2.projectES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
